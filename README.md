61ad660e6f95a5aceaadfc91ca9278383786da56
tetsu555 <tetete1118@gmail.com>
2022-05-07 16:26:28 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node-rabbitmq
+
+setup
+- [cloudamqp](https://customer.cloudamqp.com/)でインスタンスを立てる
+- インスタンスの詳細画面にアクセスしてAMQP URLをコピーする
+- getChannelのCONN_URLを書き換える
--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,5 @@
 const getChannel = async () => {
-  const CONN_URL = 'amqps://nrkscbaq:mmVTS_4A85fOU9eWHm7ZwWVliS_5JSnn@mustang.rmq.cloudamqp.com/nrkscbaq'
+  const CONN_URL = '書き換える'
   const connection = await amqp.connect(CONN_URL);
   const channel = await connection.createChannel();
 
